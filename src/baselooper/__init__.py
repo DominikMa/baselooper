a7eaba0583a52553b7690cc315148fad662b6c93
Dominik Mairhöfer <dominik@mairhoefer.xyz>
2022-02-21 13:19:50 +0100

    Fix importing

--- src/baselooper/__init__.py
+++ src/baselooper/__init__.py
@@ -3,4 +3,5 @@ VERSION = "0.0.1"
 from baselooper.state import State
 from baselooper.module import Module, ModuleConfig, SeededModule, SeededModuleConfig, NOP, NOPConfig, ModuleList, ModuleListConfig
 from baselooper.looper import Looper, LooperConfig, LooperState, LooperIterationStop, LooperIterationStopConfig
-from baselooper import logging
+
+import baselooper.logging
