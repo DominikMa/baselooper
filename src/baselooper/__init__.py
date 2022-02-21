7ea46abf289121c729d22a97fc80c7cbc5e5139d
Dominik Mairhöfer <dominik@mairhoefer.xyz>
2022-02-21 13:25:21 +0100

    Bump version

--- src/baselooper/__init__.py
+++ src/baselooper/__init__.py
@@ -1,4 +1,4 @@
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 from baselooper.state import State
 from baselooper.module import Module, ModuleConfig, SeededModule, SeededModuleConfig, NOP, NOPConfig, ModuleList, ModuleListConfig
